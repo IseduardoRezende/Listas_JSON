6056905f0e4a9005a6401db7c0f0504ff96a4c47
Eduardo Rezende <dudumoises2005@gmail.com>
2022-08-16 15:36:55 -0300

    Adicionar arquivos de projeto.

---
+++ Listas_JSON/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+
+namespace Listas_JSON
+{
+    class Program
+    {
+        enum Menu{Listar = 1, Pesquisar , Sair}
+        static void Main(string[] args)
+        {
+            bool escolheuSair = false;
+
+            while (escolheuSair == false)
+            {
+                Console.WriteLine("Sistema de Tarefas e converção JSON para C#\n");
+                Console.WriteLine("1-Listar todas Tarefas\n2-Pesquisar Tarefa\n3-Sair\n");
+                string opStr = Console.ReadLine();
+                int opInt = int.Parse(opStr);
+                Menu escolha = (Menu)opInt;
+
+                if (opInt > 0 && opInt < 4)
+                {
+                    switch (escolha)
+                    {
+                        case Menu.Listar:
+                            ReqLista();
+                            break;
+                        case Menu.Pesquisar:
+                            Requnica();
+                            break;
+                        case Menu.Sair:
+                            escolheuSair = true;
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Erro, tente novamente !");
+                    Console.ReadLine();
+                    escolheuSair = true;
+                }
+
+                Console.Clear();
+            }
+        }
+        static void Requnica()
+        {
+                Console.WriteLine("Selecione o N° do ID que deseja pesquisar:\n ");
+                string id = Console.ReadLine();
+
+                var requisicao = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/"+id);
+                requisicao.Method = "GET";
+                
+                var resposta = requisicao.GetResponse();
+                using (resposta)
+                {
+                    var stream = resposta.GetResponseStream();
+                    StreamReader leitor = new StreamReader(stream);
+                    object dados = leitor.ReadToEnd();
+
+                    Tarefa tarefas = JsonConvert.DeserializeObject<Tarefa>(dados.ToString());
+                    tarefas.Exibir();
+
+                    stream.Close();
+                    resposta.Close();
+
+                    Console.ReadLine();
+                }
+            }
+        static void ReqLista()
+        {
+            var requisicao = WebRequest.Create("https://jsonplaceholder.typicode.com/todos");
+            requisicao.Method = "GET";
+
+            var resposta = requisicao.GetResponse();
+            using (resposta)
+            {
+                var stream = resposta.GetResponseStream();
+                StreamReader leitor = new StreamReader(stream);
+                object dados = leitor.ReadToEnd();
+
+                List<Tarefa> tarefas = JsonConvert.DeserializeObject<List<Tarefa>>(dados.ToString());
+
+                foreach (Tarefa tarefa in tarefas)
+                {
+                    tarefa.Exibir();
+                }
+
+                Console.WriteLine(tarefas);
+
+                stream.Close();
+                resposta.Close();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Listas_JSON/Tarefa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas_JSON
+{
+    class Tarefa
+    {
+        public int userId;
+        public int id;
+        public string title;
+        public bool completed;
+
+        public void Exibir()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Tarefas:");
+            Console.WriteLine($"User ID: {userId}");
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Título: {title}");
+            Console.WriteLine($"Finalizou ? {completed}");
+            Console.WriteLine("");
+            Console.WriteLine("==============================");
+        }
+    }
+}
